1039907f7ce4282d52f40e7d89e9a3d31a579dec
N1ph1l1m <zerorad@inbox.ru>
2023-06-26 16:38:54 +0300

    Add activity - brainTrain

---
+++ app/src/main/java/com/example/databaseroom/brainTrain.java
@@ -0,0 +1,15 @@
+
+package com.example.databaseroom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class brainTrain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_brain_train);
+    }
+}
