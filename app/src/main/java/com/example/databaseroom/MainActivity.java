434946dfc9b12644d84b4f73433c063dd0eafa2b
N1ph1l1m <zerorad@inbox.ru>
2023-06-26 15:40:19 +0300

    Add SharedPreferences

--- app/src/main/java/com/example/databaseroom/MainActivity.java
+++ app/src/main/java/com/example/databaseroom/MainActivity.java
@@ -2,13 +2,40 @@ package com.example.databaseroom;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private SharedPreferences pref;
+    private EditText editSave;
+    private final String setKey = "save_key";
+    private TextView getText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+        getText = findViewById(R.id.getTextView);
+    }
+
+    public void save(View view) {
+        SharedPreferences.Editor edit = pref.edit();
+        edit.putString(setKey, editSave.getText().toString());
+        edit.apply();
+    }
+
+    public void get(View view) {
+        getText.setText(pref.getString(setKey,"Text"));
+    }
+
+    private void init(){
+        pref = getSharedPreferences("Test" , MODE_PRIVATE);
+        editSave = findViewById(R.id.editText);
     }
 }
