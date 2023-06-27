c71a23a56eafd4872cc4c05729685e8f8c1963ba
N1ph1l1m <zerorad@inbox.ru>
2023-06-27 15:52:59 +0300

    Update layout in brainActivity

--- app/src/main/java/com/example/databaseroom/brainTrain.java
+++ app/src/main/java/com/example/databaseroom/brainTrain.java
@@ -1,17 +1,23 @@
 
 package com.example.databaseroom;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.SharedPreferences;
-import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toolbar;
 
 public class brainTrain extends AppCompatActivity {
 
-    private Button buttonSave;
-    private Button buttonGet;
+    private Button buttonTrue;
+    private Button buttonFalse;
+    private TextView question;
+    private TextView timer;
+    private ActionBar actionBar;
     private SharedPreferences pref;
     private final String save_key = "save_key";
 
@@ -27,5 +33,18 @@ public class brainTrain extends AppCompatActivity {
 
     private void init(){
         pref = getSharedPreferences("Test" , MODE_PRIVATE);
+        timer = findViewById(R.id.timer);
+        question = findViewById(R.id.textViewQuestion);
+        actionBar = getSupportActionBar();
+        assert actionBar != null;
+        actionBar.setTitle("Question #1");
     }
+
+    public void clickTrue(View view) {
+    }
+
+    public void clickFalse(View view) {
+    }
+
+
 }
