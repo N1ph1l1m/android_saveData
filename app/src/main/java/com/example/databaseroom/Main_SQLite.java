da5b370f13031f3a0cc7d8b992d32ab38c26563c
N1ph1l1m <zerorad@inbox.ru>
2023-06-29 09:52:55 +0300

    Create layout in Main_SQLite and add event onClickDB

--- app/src/main/java/com/example/databaseroom/Main_SQLite.java
+++ app/src/main/java/com/example/databaseroom/Main_SQLite.java
@@ -3,14 +3,36 @@ package com.example.databaseroom;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Main_SQLite extends AppCompatActivity {
 
+    private TextView  textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_sqlite);
+        textView = findViewById(R.id.textView);
+    }
+    public void onClickDB(View view) {
+        SQLiteDatabase db = getBaseContext().openOrCreateDatabase("app.db",MODE_PRIVATE,null);
+        db.execSQL("CREATE TABLE IF NOT EXISTS users(name TEXT, age INTEGER, UNIQUE(name))");
+        db.execSQL("INSERT OR IGNORE INTO users VALUES ('TOM Smith',23),('John Dow',32);");
+        Cursor query = db.rawQuery("SELECT * FROM users;",null);
+        textView.setText("");
+        while(query.moveToNext()){
+            String name = query.getString(0);
+            int age = query.getInt(1);
+            textView.append("Name: " + name +"\n"+ "Age:" + age +"\n");
+        }
+        query.close();
+        db.close();
+
+
     }
 
 
@@ -22,4 +44,6 @@ public class Main_SQLite extends AppCompatActivity {
         startActivity(intent);
         finish();
     }
+
+
 }
