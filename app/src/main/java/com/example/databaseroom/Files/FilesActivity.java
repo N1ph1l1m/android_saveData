5ee5c48b08bafe77d10cfd3d50bbec04ca58c787
N1ph1l1m <zerorad@inbox.ru>
2023-07-12 16:32:55 +0300

    Create activity FilesActivity

---
+++ app/src/main/java/com/example/databaseroom/Files/FilesActivity.java
@@ -0,0 +1,16 @@
+package com.example.databaseroom.Files;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.databaseroom.R;
+
+public class FilesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_files);
+    }
+}
